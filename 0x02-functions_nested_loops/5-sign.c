04aa44a95dd35f99aa9bf5207de1176cdd0d6bbf
Harobed1724 <deborahakoranteng@gmail.com>
2022-03-16 02:18:06 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,12 +1,10 @@
-
-#include "holberton.h"
+#include "main.h"
 
 /**
- * print_sign - prints the sign of a number
- * @n: the int to check
- * Return: 1 and prints + if n is greater than zero
- * 0 and prints 0 if n is zero
- * -1 and prints - if n is less than zero
+ * print_sign - print the sign of numbers
+ * @n: the number that is being tested
+ *
+ * Return: 0 is sucess and 1
  */
 int print_sign(int n)
 {
@@ -14,13 +12,16 @@ int print_sign(int n)
 	{
 		_putchar('+');
 		return (1);
-	} else if (n == 0)
+	}
+	else if (n == 0)
 	{
-		_putchar(48);
+		_putchar('0');
 		return (0);
-	} else if (n < 0)
+	}
+	else if (n < 0)
 	{
 		_putchar('-');
-	}
 		return (-1);
+	}
+	return (0);
 }
