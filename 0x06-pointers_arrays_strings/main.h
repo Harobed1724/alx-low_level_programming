7a09f0c0c5336263a43cef40e00db98035632172
Harobed1724 <deborahakoranteng@gmail.com>
2022-03-24 04:17:02 -0700

    ayy

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef main.h
-#define main.h
+#ifndef FILE_MAIN
+#define FILE_MAIN
 
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -11,7 +11,7 @@ void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
 int _atoi(char *s);
 
 #endif
